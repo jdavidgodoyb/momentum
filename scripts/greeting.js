f3c9c6129f1d396b684de8bc30e87373f41d7c07
jdavidgodoyb <53903605+jdavidgodoyb@users.noreply.github.com>
2019-08-27 15:06:30 -0400

    make momentum

---
+++ scripts/greeting.js
@@ -0,0 +1,26 @@
+const form = document.querySelector('.js-form');
+const input = form.querySelector('input');
+const greeting = document.querySelector('.js-greetings');
+const USER_LS = 'currentUser';
+const SHOWING_CN = 'showing';
+
+function paintGreeting(text){
+form.classList.remove(SHOWING_CN);
+greeting.classList.add(SHOWING_CN);
+greeting.innerText = `Hola! ${text}`;
+}
+
+function loadName() {
+    const currentUser = localStorage.getItem(USER_LS);
+    if (currentUser === null){
+        askForName();
+    }else {
+        paintGreeting(currentUser);
+    }
+}
+
+function init() {
+    loadName();
+}
+
+init();
